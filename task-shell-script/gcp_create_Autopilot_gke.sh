c47292fd089806f01744ec945a63e5bd4ed571cb
Max Huang <sakana@cycu.org.tw>
2024-03-03 00:26:39 +0800

    update create script with release channel and GKE version

--- task-shell-script/gcp_create_Autopilot_gke.sh
+++ task-shell-script/gcp_create_Autopilot_gke.sh
@@ -23,11 +23,32 @@ echo ""
 read -e -p "Please enter cluster name: " -i "test-cluster" cluster_name
 echo ""
 
+# 顯示目前可用 GKE 版本
+echo ""
+echo "顯示目前可用 GKE 版本"
+echo ""
+gcloud container get-server-config  --project $project_name --format json | jq '.channels'
+echo ""
+
+# 設定 Release Channel
+echo ""
+echo "設定 Release Channel"
+echo ""
+read -e -p "Please enter Release Channel: " -i "regular" release_channel_name
+echo ""
+
+# 設定 GKE 版本
+echo ""
+echo "設定 GKE 版本"
+echo ""
+read -e -p "Please enter GKE version: " -i "1.28" gke_version
+echo ""
+
 # 建立 Autopilot GKE cluster
 echo ""
 echo "建立 Autopilot GKE cluster"
 echo "大概需要 5 - 10 分鐘建立"
-gcloud container clusters create-auto $cluster_name   --region $region_name --project $project_name
+gcloud container clusters create-auto $cluster_name   --region $region_name --release-channel $release_channel_name --cluster-version $gke_version --project $project_name
 echo ""
 
 # 詢問是否要下載 GKE Credentials
